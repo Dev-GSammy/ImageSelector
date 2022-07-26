f32d1f5dfc43b684e19dd1420b4255e8abab0693
Dev-GSammy <samueltaiwofatunmbi1@gmail.com>
2022-07-26 12:02:19 +0100

    Add project files.

---
+++ ImageSelector/ImageSelector/MainPage.xaml.cs
@@ -0,0 +1,43 @@
+﻿using Plugin.Media;
+using Plugin.Media.Abstractions;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace ImageSelector
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        private async void Button_Clicked(object sender, EventArgs e)
+        {
+            await CrossMedia.Current.Initialize();
+            //check if picking pictures is supported
+            if (!CrossMedia.Current.IsPickPhotoSupported)
+            {
+                await DisplayAlert("Pick Photo", "Picture picking is not supported", "Ok");
+                return;
+            }
+            //To reduce picture size. Default is a very large picture
+            var MediaOptions = new PickMediaOptions()
+            {
+                PhotoSize = PhotoSize.Medium
+            };
+            //Pick photo from gallery
+            var selectedImageFile = await CrossMedia.Current.PickPhotoAsync(MediaOptions);
+            if (selectedImageFile == null)
+            {
+                await DisplayAlert("Pick Photo", "The picture you chose is null, please pick another", "Ok");
+            }
+            SelectedImage.Source = ImageSource.FromStream(() => selectedImageFile.GetStream());
+        }
+    }
+}
